43a37b63ef3c2952ba13530022d0b0ae40611c71
TrickShotMLG02 <tim.schlachter@shinternet.ch>
2024-03-15 00:32:51 +0100

    added placeholder displays

--- EnergyMonitor/program/server.lua
+++ EnergyMonitor/program/server.lua
@@ -237,17 +237,17 @@ local function setupMonitor()
     -- Total EnergyMeter Transfer Display --
     ----------------------------------------
 
-    local capWidth = 30
-    local capHeight = 2
+    local trnsfWidth = 30
+    local trnsfHeight = 2
 
-    local capMinX = monWidth - btnOffsetBorder - capWidth
-    local capMinY = btnOffsetBorder
-    local capMaxX = monWidth - btnOffsetBorder
-    local capMaxY = capHeight + capMinY
+    local trnsfMinX = monWidth - btnOffsetBorder - trnsfWidth
+    local trnsfMinY = btnOffsetBorder
+    local trnsfMaxX = monWidth - btnOffsetBorder
+    local trnsfMaxY = trnsfHeight + trnsfMinY
     local lh = 3
-    
-    currentPage:add("OutputRate", function() end, capMinX, capMinY, capMaxX, capMaxY, colors.red, colors.lime)
-    currentPage:add("InputRate", function() end, capMinX, capMinY + lh, capMaxX, capMaxY + lh, colors.red, colors.lime)
+
+    currentPage:add("OutputRate", function() end, trnsfMinX, trnsfMinY, trnsfMaxX, trnsfMaxY, colors.red, colors.lime)
+    currentPage:add("InputRate", function() end, trnsfMinX, trnsfMinY + lh, trnsfMaxX, trnsfMaxY + lh, colors.red, colors.lime)
     print(totalPageCount)
 
 
@@ -262,9 +262,110 @@ local function setupMonitor()
     --SETUP PAGES and add them to page table. Also call setupMonitor in interval since the amount of cells might change
     --Buttons next/prev used to inc/dec pageIndex
 
+    local vertOffset = capMaxY + lh + 4
+    local horiOffset = 5
+
+    ---------------
+    -- DISPLAY 1 --
+    ---------------
+
+    local dpWidth = 15
+    local dpHeight = 2
+
+    dp1MinX1 = btnOffsetBorder
+    dp1MinY1 = vertOffset
+    dp1MaxX1 = dpWidth + dp1MinX1
+    dp1MaxY1 = dpHeight + dp1MinY1
+
+    dp1MinX2 = dp1MinX1
+    dp1MinY2 = dp1MaxY1 + 1
+    dp1MaxX2 = dpWidth + dp1MinX2
+    dp1MaxY2 = dpHeight + dp1MinY2
+
+    dp1MinX3 = dp1MinX2
+    dp1MinY3 = dp1MaxY2 + 1
+    dp1MaxX3 = dpWidth + dp1MinX3
+    dp1MaxY3 = dpHeight + dp1MinY3
+
+    currentPage:add("Display1Name", function() end, dp1MinX1, dp1MinY1, dp1MaxX1, dp1MaxY1, colors.red, colors.lime)
+    currentPage:add("Display1Rate", function() end, dp1MinX2, dp1MinY2, dp1MaxX2, dp1MaxY2, colors.red, colors.lime)
+    currentPage:add("Display1State", function() end, dp1MinX3, dp1MinY3, dp1MaxX3, dp1MaxY3, colors.red, colors.lime)
 
 
-    
+    ---------------
+    -- DISPLAY 2 --
+    ---------------
+
+    dp2MinX1 = dp1MaxX1 + horiOffset
+    dp2MinY1 = vertOffset
+    dp2MaxX1 = dpWidth + dp2MinX1
+    dp2MaxY1 = dpHeight + dp2MinY1
+
+    dp2MinX2 = dp2MinX1
+    dp2MinY2 = dp2MaxY1 + 1
+    dp2MaxX2 = dpWidth + dp2MinX2
+    dp2MaxY2 = dpHeight + dp2MinY2
+
+    dp2MinX3 = dp2MinX2
+    dp2MinY3 = dp2MaxY2 + 1
+    dp2MaxX3 = dpWidth + dp2MinX3
+    dp2MaxY3 = dpHeight + dp2MinY3
+
+    currentPage:add("Display2Name", function() end, dp2MinX1, dp2MinY1, dp2MaxX1, dp2MaxY1, colors.red, colors.lime)
+    currentPage:add("Display2Rate", function() end, dp2MinX2, dp2MinY2, dp2MaxX2, dp2MaxY2, colors.red, colors.lime)
+    currentPage:add("Display2State", function() end, dp2MinX3, dp2MinY3, dp2MaxX3, dp2MaxY3, colors.red, colors.lime)
+
+
+    ---------------
+    -- DISPLAY 3 --
+    ---------------
+
+    dp3MinX1 = dp2MaxX1 + horiOffset
+    dp3MinY1 = vertOffset
+    dp3MaxX1 = dpWidth + dp3MinX1
+    dp3MaxY1 = dpHeight + dp3MinY1
+
+    dp3MinX2 = dp3MinX1
+    dp3MinY2 = dp3MaxY1 + 1
+    dp3MaxX2 = dpWidth + dp3MinX2
+    dp3MaxY2 = dpHeight + dp3MinY2
+
+    dp3MinX3 = dp3MinX2
+    dp3MinY3 = dp3MaxY2 + 1
+    dp3MaxX3 = dpWidth + dp3MinX3
+    dp3MaxY3 = dpHeight + dp3MinY3
+
+    currentPage:add("Display3Name", function() end, dp3MinX1, dp3MinY1, dp3MaxX1, dp3MaxY1, colors.red, colors.lime)
+    currentPage:add("Display3Rate", function() end, dp3MinX2, dp3MinY2, dp3MaxX2, dp3MaxY2, colors.red, colors.lime)
+    currentPage:add("Display3State", function() end, dp3MinX3, dp3MinY3, dp3MaxX3, dp3MaxY3, colors.red, colors.lime)
+
+
+    ---------------
+    -- DISPLAY 4 --
+    ---------------
+
+    dp4MinX1 = dp3MaxX1 + horiOffset
+    dp4MinY1 = vertOffset
+    dp4MaxX1 = dpWidth + dp4MinX1
+    dp4MaxY1 = dpHeight + dp4MinY1
+
+    dp4MinX2 = dp4MinX1
+    dp4MinY2 = dp4MaxY1 + 1
+    dp4MaxX2 = dpWidth + dp4MinX2
+    dp4MaxY2 = dpHeight + dp4MinY2
+
+    dp4MinX3 = dp4MinX2
+    dp4MinY3 = dp4MaxY2 + 1
+    dp4MaxX3 = dpWidth + dp4MinX3
+    dp4MaxY3 = dpHeight + dp4MinY3
+
+    currentPage:add("Display4Name", function() end, dp4MinX1, dp4MinY1, dp4MaxX1, dp4MaxY1, colors.red, colors.lime)
+    currentPage:add("Display4Rate", function() end, dp4MinX2, dp4MinY2, dp4MaxX2, dp4MaxY2, colors.red, colors.lime)
+    currentPage:add("Display4State", function() end, dp4MinX3, dp4MinY3, dp4MaxX3, dp4MaxY3, colors.red, colors.lime)
+
+
+
+
     ---------------------------------------
     -- footer buttons offsets/dimensions --
     ---------------------------------------
