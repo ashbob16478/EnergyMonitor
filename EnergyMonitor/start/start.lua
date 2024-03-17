389918f92bf8fc322006869c01a0213c4513129c
TrickShotMLG02 <tim.schlachter@shinternet.ch>
2024-03-17 19:19:39 +0100

    improved update detection

--- EnergyMonitor/start/start.lua
+++ EnergyMonitor/start/start.lua
@@ -140,11 +140,15 @@ function _G.checkUpdates()
     if remoteVer == nil then
 		print("Couldn't get remote version from gitlab.")
 	else
+		-- only used to check for update since eg. 1.1-XXX > 1.1.5-XXX
+		vNum = string.sub(version, 0, string,find(version, "-")-1)
+		rvNum = string.sub(remoteVer, 0, string,find(remoteVer, "-")-1)
+
 		print("remoteVer: "..remoteVer)
-		print("Update? -> "..tostring(remoteVer > version))
-	
+		print("Update? -> "..tostring(rvNum > vNum))
+		
 	    --Update if available
-	    if remoteVer > version then
+	    if rvNum > vNum then
 		    print("Update...")
 		    sleep(2)
 		    doUpdate(remoteVer,currBranch)
